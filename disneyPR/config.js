95bd63c20df67851f4d8d9540b0f60ed34f549dd
Lapis <lapis.partisan1213@gmail.com>
2025-05-08 07:23:35 +0800

    Add files via upload

---
+++ disneyPR/config.js
@@ -0,0 +1,5 @@
+const config = {
+    localStoragePrefix: "nousagi-disney-songs-party-rank-sorter-may25",
+    title: "Nousagis Disney Songs Party Rank Sorter",
+    description: "Nousagis party rank sorter for disney songs."
+};
