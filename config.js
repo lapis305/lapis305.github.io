1291fc1f0f98ed4eec8866ed1d442dd1b36e3b65
Lapis <lapis.partisan1213@gmail.com>
2025-05-08 07:18:04 +0800

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-const config = {
-    localStoragePrefix: "nousagi-anisong-2000-party-rank-sorter",
-    title: "Nousagis Anisong 2000 Party Rank Sorter",
-    description: "Nousagis Party rank sorter for year 2000 anime songs."
-};
