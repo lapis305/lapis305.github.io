f1193eb3d39e5070902b7546ca7e7127519452c0
Lapis <lapis.partisan1213@gmail.com>
2025-05-20 03:39:57 +0800

    Update config.js

--- anisong2002PR/config.js
+++ anisong2002PR/config.js
@@ -1,5 +1,5 @@
 const config = {
-    localStoragePrefix: "nousagi-2001-anisong-party-rank-sorter-may25",
-    title: "Nousagis 2001 Anisong Party Rank Sorter",
-    description: "Nousagis party rank sorter for 2001 anisongs."
+    localStoragePrefix: "nousagi-2002-anisong-party-rank-sorter-may25",
+    title: "Nousagis 2002 Anisong Party Rank Sorter",
+    description: "Nousagis party rank sorter for 2002 anisongs."
 };
