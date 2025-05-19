1add077d937116b3a9ba448c2481baaa38b8337f
Muhammad Haziq bin Azman <lapis.partisan1213@gmail.com>
2025-05-20 03:46:50 +0800

    Archive 2001PR

--- anisong2001PR/config.js
+++
@@ -1,5 +0,0 @@
-const config = {
-    localStoragePrefix: "nousagi-2001-anisong-party-rank-sorter-may25",
-    title: "Nousagis 2001 Anisong Party Rank Sorter",
-    description: "Nousagis party rank sorter for 2001 anisongs."
-};
