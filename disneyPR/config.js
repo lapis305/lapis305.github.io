282b200edf3ff588d392ab15366d748493830ef1
Muhammad Haziq bin Azman <lapis.partisan1213@gmail.com>
2025-05-30 01:20:36 +0800

    EDM PR
    Archived old PRs and created the EDM PR sorter

--- disneyPR/config.js
+++
@@ -1,5 +0,0 @@
-const config = {
-    localStoragePrefix: "nousagi-disney-songs-party-rank-sorter-may25",
-    title: "Nousagis Disney Songs Party Rank Sorter",
-    description: "Nousagis party rank sorter for disney songs."
-};
---
+++ edmPR/config.js
@@ -0,0 +1,5 @@
+const config = {
+    localStoragePrefix: "nousagi-edm-party-rank-sorter-may25",
+    title: "Nousagis EDM Party Rank Sorter",
+    description: "Nousagis party rank sorter for EDM songs."
+};
