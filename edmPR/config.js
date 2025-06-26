5f11ea39c1c49fb39de631423a2b92b0ab853f8b
Muhammad Haziq bin Azman <lapis.partisan1213@gmail.com>
2025-06-27 01:08:18 +0800

    Updated for touhou PR

--- edmPR/config.js
+++
@@ -1,5 +0,0 @@
-const config = {
-    localStoragePrefix: "nousagi-edm-party-rank-sorter-may25",
-    title: "Nousagis EDM Party Rank Sorter",
-    description: "Nousagis party rank sorter for EDM songs."
-};
---
+++ touhouPR/config.js
@@ -0,0 +1,5 @@
+const config = {
+    localStoragePrefix: "nousagi-touhou-party-rank-sorter-june25",
+    title: "Nousagis Touhou Party Rank Sorter",
+    description: "Nousagis party rank sorter for touhou songs."
+};
