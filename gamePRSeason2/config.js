395a7266977561285098746ed6c74b32fdd8dd3c
Muhammad Haziq bin Azman <lapis.partisan1213@gmail.com>
2025-09-14 15:00:58 +0800

    Updated for Game PR

---
+++ gamePRSeason2/config.js
@@ -0,0 +1,5 @@
+const config = {
+    localStoragePrefix: "nousagi-gamePR-season2-sept25",
+    title: "Nousagis Game Songs Party Rank Sorter",
+    description: "Nousagis party rank sorter for game songs."
+};
--- westernPRseason2/config.js
+++
@@ -1,5 +0,0 @@
-const config = {
-    localStoragePrefix: "nousagi-western-season2-party-rank-sorter-july25",
-    title: "Nousagis Western Songs Season 2 Party Rank Sorter",
-    description: "Nousagis party rank sorter for western songs (season 2)."
-};
